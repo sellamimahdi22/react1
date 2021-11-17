e147ec8de81992f0e10c9795a9409768f5f7ac7e
sellamimahdi22 <sellamimahdi.ms23@gmail.com>
2021-11-17 09:33:38 +0100

    first

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+import  Check  from './comp/Check';
+
+function App() {
+  return (
+    <div className="App">
+     
+        <Check />
+    </div>
+  );
+}
+
+export default App;
